d32ffa3329db790c769f285ab6db1ca17a79d4aa
Rob Wymer <wymer1120@gmail.com>
2017-07-19 16:16:37 -0400

    First

---
+++ ToDo CoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  ToDo CoreData
+//
+//  Created by Alex Wymer  on 2017-07-19.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ ToDo CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDo CoreData
+//
+//  Created by Alex Wymer  on 2017-07-19.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
